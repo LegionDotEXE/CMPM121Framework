ce2cb34b55f747a581835a8ba00023c5a3ab97b3
Saurab S <ajay.v.v.743@gmail.com>
2026-05-22 20:17:38 -0700

    PlayerClasses + ClassSelectorMenu updates

--- Assets/Scripts/Levels/ClassInfo.cs
+++ Assets/Scripts/Levels/ClassInfo.cs
@@ -6,18 +6,29 @@ using System.Linq;
 
 public class ClassInfo
 {
-    public List<JToken> classes; 
+    public List<JToken> classes;
+    public Dictionary<string, JToken> classData;
+    public string selectedClass = "mage";
+
     private static ClassInfo theInstance;
-    public static ClassInfo Instance {  get 
+    public static ClassInfo Instance
+    {
+        get
         {
             if (theInstance == null)
                 theInstance = new ClassInfo();
-            return theInstance; 
-        } 
+            return theInstance;
+        }
     }
 
     private ClassInfo()
     {
         classes = JToken.Parse(File.ReadAllText("./Assets/Resources/classes.json")).Children().Values().ToList();
+        JObject raw = JObject.Parse(File.ReadAllText("./Assets/Resources/classes.json"));
+        classData = new Dictionary<string, JToken>();
+        foreach (var entry in raw)
+        {
+            classData[entry.Key] = entry.Value;
+        }
     }
 }
